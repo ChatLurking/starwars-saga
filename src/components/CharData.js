a33e7e08a127ec9a096c20ba9c2635c40d6bf125
ChatLurking <anneglines@gmail.com>
2017-04-09 20:08:03 -0700

    Added font awesome icon

--- src/components/CharData.js
+++ src/components/CharData.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import FontAwesome from 'react-fontawesome';
+import 'font-awesome/css/font-awesome.min.css';
 
 const Wrapper = styled.div`
   display: flex;
@@ -23,13 +25,25 @@ const Span = styled.span`
   margin: 0 10px 0 0;
 `;
 
+const NameIcon = styled.div`
+  display: flex;
+`;
 
 const CharData = (props) => {
   if(props.charData) {
     return (
       <Wrapper>
-        <H2>Name: {props.charData.name}</H2>
-        <H3>Birth Year: {props.charData.birthYear}</H3>
+        <NameIcon>
+          <FontAwesome
+            style={{marginRight: '10px'}}
+            name='id-badge'
+            size='5x'
+            />
+          <Wrapper style={{ justifyContent: 'space-around' }}>
+            <H2>Name: {props.charData.name}</H2>
+            <H3>Birth Year: {props.charData.birthYear}</H3>
+          </Wrapper>
+        </NameIcon>
         <P><Span>Hair Color: {props.charData.hairColor} </Span><Span> Eye Color: {props.charData.eyeColor}</Span></P>
         <p>Gender: {props.charData.gender}</p>
         <P><Span>Height: {props.charData.height}</Span><Span> Mass: {props.charData.mass}</Span></P>
